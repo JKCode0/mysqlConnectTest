37a24f11b2f3d2d468e1d10276236bca90f9d5f5
speedDev <speedDev@speedDevCom>
2016-09-11 16:04:37 +0900

    commit

---
+++ mysql_connect/src/java/com/jk/xteam/entity/SchemaEntity.java
@@ -0,0 +1,29 @@
+package com.jk.xteam.entity;
+
+/**
+ *  tableName
+ * @author speedDev
+ *
+ */
+public class SchemaEntity {
+	
+	private String tableName;
+	private String database;
+
+	public String getDatabase() {
+		return database;
+	}
+
+	public void setDatabase(String database) {
+		this.database = database;
+	}
+
+	public String getTableName() {
+		return tableName;
+	}
+
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+	
+}
--- mysql_connect/src/test/test/com/jk/db/test/QueryManagerTest.java
+++ mysql_connect/src/test/test/com/jk/db/test/QueryManagerTest.java
@@ -1,11 +1,15 @@
 package test.com.jk.db.test;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.jk.xteam.entity.SchemaEntity;
 
 import test.com.jk.util.DBSimpleConnector;
 
@@ -50,6 +54,61 @@ public class QueryManagerTest {
 	}
 
 	//
+	// show full columns from 'tb_menu' <-- 호출안됨.
+	@Test
+	public void getTableColumns(){
+		SqlSession sqlSession = null;
+		try {
+			
+			sqlSession = dbConn.getSession();
+			Map result = sqlSession.selectMap("selectShowColumns", "TB_MENU");
+			//
+			System.out.println("result:" + result);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			if(sqlSession!=null){
+				sqlSession.close();
+			}
+		}
+		
+	} 
+	
+	//
+	// columns 확인
+	@Test
+	public void getColumns(){
+		SqlSession sqlSession = null;
+		try {
+			
+			sqlSession = dbConn.getSession();
+			
+			SchemaEntity schemaEntity = new SchemaEntity();
+			schemaEntity.setDatabase("DEV_DB");
+			schemaEntity.setTableName("TB_MENU");
+			
+			List<Map> list = sqlSession.selectList("selectColumns", schemaEntity);
+			//
+			System.out.println("list len:" + list.size());
+			
+			int cnt = 0;
+			for(Map m: list){
+				++cnt;
+				System.out.println("["+cnt+"]" + m);
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			if(sqlSession!=null){
+				sqlSession.close();
+			}
+		}
+		
+	} 
 	
 	
 }
